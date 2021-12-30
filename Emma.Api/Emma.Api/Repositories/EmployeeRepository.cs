f736810a948223bbba2b55b5a96d1e5951715abf
Kyle Reilly <kjreills@gmail.com>
2021-12-30 15:29:33 -0500

    Create initial repo, add EF Core

---
+++ Emma.Api/Emma.Api/Repositories/EmployeeRepository.cs
@@ -0,0 +1,44 @@
+﻿using CSharpFunctionalExtensions;
+using Emma.Api.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Emma.Api.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public EmployeeRepository()
+        {
+
+        }
+
+        public Task<Result<Employee, RepositoryError>> Create(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Employee>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Maybe<Employee>> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Employee, RepositoryError>> Update(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmployeeContext : DbContext
+    {
+
+    }
+}
